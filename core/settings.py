27383332714f58122cdb9341305927869e6ce52a
Hithallo Sousa Santos <hithallosantos@gmail.com>
2025-02-04 21:20:27 -0300

    Ajuste settings

--- core/settings.py
+++ core/settings.py
@@ -99,7 +99,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        engine='django.db.backends.postgresql'
+    )
 }
 
 # PASSWORD_HASHERS = [
